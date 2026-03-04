a744a8a1e5cff08903584936ec793f6e10e654b1
Vedant Chainani <vedantchainani1084@gmail.com>
2026-03-04 21:41:25 +0530

    chore: ad helper to derive config type

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -1,9 +1,9 @@
-import { Config, type Option, type Redacted } from "effect";
+import { Config, type Effect } from "effect";
 export const EnsClientConfig = Config.all({
   rpcUrl: Config.redacted("RPC_URL").pipe(Config.option),
   subgraphUrl: Config.redacted("SUBGRAPH_URL").pipe(Config.option),
 });
 
-export type EnsClientConfigValues = {
-  rpcUrl: Option.Option<Redacted.Redacted<string>>;
-};
+export type EnsClientConfigValues = Effect.Success<
+  ReturnType<typeof EnsClientConfig.asEffect>
+>;
